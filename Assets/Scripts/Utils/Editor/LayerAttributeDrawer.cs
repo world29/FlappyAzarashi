5d1a877e482e39e453021ea547e455cb451c926b
world29 <623p6236p@gmail.com>
2021-03-06 03:57:46 +0900

    レイヤーをインスペクタで設定しやすくするユーティリティスクリプトを追加

---
+++ Assets/Scripts/Utils/Editor/LayerAttributeDrawer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(LayerAttribute))]
+public class LayerAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+    }
+}
