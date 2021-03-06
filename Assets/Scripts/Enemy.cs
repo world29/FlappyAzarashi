162689991385b1f024196e3f5c089597fceb25a1
world29 <623p6236p@gmail.com>
2021-03-07 06:33:17 +0900

    敵がプレイヤーの方を向くオプションを追加

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public bool m_lookatPlayer;
+    [Range(0, 90)]
+    public float m_lookatRotationLimit = 30;
+    public float m_lookatRotationSpeed = 10;
+
     GameObject gameController;
+    GameObject m_player;
 
     void Start()
     {
         gameController = GameObject.FindWithTag("GameController");
+        m_player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        // プレイヤーのほうを向く
+        if (m_lookatPlayer)
+        {
+            Quaternion targetRotation = Quaternion.identity;
+
+            Vector3 enemyDirection = Vector3.left;
+            var toPlayer = m_player.transform.position - transform.position;
+
+            var angle = Vector3.SignedAngle(enemyDirection, toPlayer, Vector3.forward);
+
+            bool inSight = (angle < m_lookatRotationLimit && angle > -m_lookatRotationLimit);
+            if (inSight)
+            {
+                targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+
+            var step = m_lookatRotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,4 +55,26 @@ public class Enemy : MonoBehaviour
 
         gameObject.SetActive(false);
     }
+
+#if DEBUG
+    private void OnDrawGizmos()
+    {
+        var handlePos = transform.position;
+
+        UnityEditor.Handles.color = new Color(1, 0, 0, 0.2f);
+        UnityEditor.Handles.DrawSolidArc(
+            handlePos,
+            Vector3.back,
+            Vector3.left,
+            m_lookatRotationLimit,
+            UnityEditor.HandleUtility.GetHandleSize(handlePos));
+
+        UnityEditor.Handles.DrawSolidArc(
+            handlePos,
+            Vector3.back,
+            Vector3.left,
+            -m_lookatRotationLimit,
+            UnityEditor.HandleUtility.GetHandleSize(handlePos));
+    }
+#endif
 }
