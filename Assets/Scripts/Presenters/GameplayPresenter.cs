28da5bc257da286c0f84c732f603d25b5b3d9d3f
world29 <623p6236p@gmail.com>
2021-04-29 23:15:59 +0900

    リファクタリング

---
+++ Assets/Scripts/Presenters/GameplayPresenter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayPresenter : MonoBehaviour, IGameStateEventHandler
+{
+    public GameObject m_gameplayUI;
+
+    private void OnEnable()
+    {
+        BroadcastReceivers.RegisterBroadcastReceiver<IGameStateEventHandler>(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        BroadcastReceivers.UnregisterBroadcastReceiver<IGameStateEventHandler>(gameObject);
+    }
+
+    public void OnGameStateChanged(IGameState prevState, IGameState nextState)
+    {
+        // Ready -> Gameplay ステートの間は UI を表示する
+        if (nextState is GameState_GameplayReady)
+        {
+            OnEnterReadyState();
+        }
+
+        if (prevState is GameState_GameplayMain)
+        {
+            OnExitGameplayState();
+        }
+    }
+
+    void OnEnterReadyState()
+    {
+        m_gameplayUI.SetActive(true);
+    }
+
+    void OnExitGameplayState()
+    {
+        m_gameplayUI.SetActive(false);
+    }
+}
