e327af7c1fc284cabf11d8243e8fcaed19168357
world29 <623p6236p@gmail.com>
2021-05-01 18:00:10 +0900

    テストモードによる広告が表示されるようにした

---
+++ Assets/Scripts/Advertisement/InitializeAdsScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour
+{
+    static readonly string kGameID_iOS = "4112076";
+    static readonly string kGameID_Android = "4112077";
+
+    string gameId = kGameID_iOS;
+    bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+}
