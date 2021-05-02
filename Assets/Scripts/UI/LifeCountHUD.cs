4d782c8c97519176bd50b9077c72a1e85c98e8f9
world29 <623p6236p@gmail.com>
2021-05-02 17:35:06 +0900

    残機の UI を変更

---
+++ Assets/Scripts/UI/LifeCountHUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class LifeCountHUD : MonoBehaviour
+{
+    public TextMeshProUGUI m_lifeText;
+
+    private void OnEnable()
+    {
+        GameDataAccessor.OnPlayerLifeCountChanged.AddListener(UpdateUI);
+
+        UpdateUI(GameDataAccessor.PlayerLifeCount);
+    }
+
+    private void OnDisable()
+    {
+        GameDataAccessor.OnPlayerLifeCountChanged.RemoveListener(UpdateUI);
+    }
+
+    public void UpdateUI(int lifeCount)
+    {
+        m_lifeText.text = "x " + lifeCount;
+    }
+}
