dd706d40ca39e9060a26dcf1ce483231a0e19535
world29 <623p6236p@gmail.com>
2021-05-03 01:33:48 +0900

    タイトル画面の START ボタンの点滅を DOTween による実装に置き換え

---
+++ Assets/Scripts/UI/BlinkCanvasGroup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class BlinkCanvasGroup : MonoBehaviour
+{
+    public float m_durationSeconds;
+
+    public Ease m_easeType;
+
+    private CanvasGroup m_canvasGroup;
+
+    private void Start()
+    {
+        m_canvasGroup = GetComponent<CanvasGroup>();
+
+        m_canvasGroup
+            .DOFade(0.0f, m_durationSeconds)
+            .SetEase(m_easeType)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
