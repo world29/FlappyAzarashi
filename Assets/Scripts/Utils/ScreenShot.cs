06d2ed0739bf837613aef5d515ec369b9ce480c5
world29 <623p6236p@gmail.com>
2021-05-05 21:41:34 +0900

    スクリーンショット撮影用ヘルパースクリプトを追加

---
+++ Assets/Scripts/Utils/ScreenShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    static int count = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            count++;
+
+            ScreenCapture.CaptureScreenshot("screenshot" + count.ToString() + ".png");
+        }
+    }
+}
